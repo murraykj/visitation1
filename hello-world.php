e125d92e05e0017537210be80399d9c576fb6599
Kevin Murray <murraykj@ameritech.net>
2017-01-12 20:19:26 +0000

    visitation

---
+++ hello-world.php
@@ -0,0 +1,16 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+
+?>
+
+Hello 
+<BR>
+    <BR>
+        <a href="test.html">test web page</a>
+</body>
+</html>
